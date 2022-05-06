8d0395ca6b8dbc54a908bf6c9c986c94a347c9ec
ogoras <skus30@wp.pl>
2022-05-06 12:45:47 +0200

    back to orthogonal projection

--- camera.js
+++ camera.js
@@ -2,18 +2,12 @@ function perspectiveProject(vertex) {
     var x = vertex.x;
     var y = vertex.y;
     var z = - vertex.z;
-    var x2 = x * camera.focalLength() / z;
-    var y2 = y * camera.focalLength() / z;
-    console.log(x2, y2);
-    return {x: x2, y: y2};
+    var angle = camera.angle;
+    return {x: vertex.x/angle, y: vertex.y/angle};
 }
 
 export var camera = {
     //viewing angle
-    zoom : 10,
-    project : perspectiveProject,
-    //calculate focal length from zoom
-    focalLength : function() {
-        return (camera.zoom / 180) * Math.PI;
-    }
+    angle : 10,
+    project : perspectiveProject
 }
